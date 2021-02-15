7c6f24bbe8674da8e6f70a1d6b849cd79ae6a0ed
Bhagwat Karankar <bhagwat.karankar@synerzip.com>
2021-02-16 00:25:06 +0530

    Added redux logger setup.

---
+++ generators/app/templates/src/redux/store/configStore.js
@@ -0,0 +1,17 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
+
+const logger = createLogger();
+
+export default (initialState = {}) => {
+  const middleware = [ReduxThunk, logger];
+  const middlewareEnhancer = composeWithDevTools(
+    applyMiddleware(...middleware)
+  );
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = compose(...enhancers);
+  return composedEnhancers;
+};
